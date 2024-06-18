87ab6d6247285f7649a1c4a1d08abb609e18abe8
Sanan Rao <raosanan@gmail.com>
2024-06-18 16:53:16 -0400

    Squashed commit of the following:
    commit 147ca6ba25ff2d645c7be62704dc1d5e46f5c45a
    Author: Sanan Rao <raosanan@gmail.com>
    Date:   Tue Jun 18 16:46:27 2024 -0400
    
        Small fixes
    
    commit 22e5ff0a7c24d58a1a3cca3034b79687c19eb9fd
    Merge: a70206a 821eaec
    Author: Sanan Rao <raosanan@gmail.com>
    Date:   Tue Jun 18 01:18:34 2024 -0400
    
        Merge remote-tracking branch 'origin/init/bootstrap' into landing-page
    
    commit a70206a6d63724ef0066d6715d28f71abcf37b56
    Author: SananR <raosanan@gmail.com>
    Date:   Mon Jun 17 04:04:41 2024 -0400
    
        Routing
    
    commit 8e46b2e3c5e0c9692588e472d9208a6db5a3bc09
    Author: Sanan Rao <raosanan@gmail.com>
    Date:   Sun Jun 9 00:30:40 2024 -0400
    
        Update logo.png
    
    commit c0890e39653f102748b04d7a222018a8a7ac1f94
    Author: Sanan Rao <raosanan@gmail.com>
    Date:   Sun Jun 9 00:25:57 2024 -0400
    
        Update logo.png
    
    commit c0153de9c62c794bdc49ed22b6cd805512bb6752
    Author: Sanan Rao <raosanan@gmail.com>
    Date:   Sun Jun 9 00:24:39 2024 -0400
    
        Logo

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SignupFormComponent } from './components/form/signup-form/signup-form.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { SignupFormComponent } from "./components/form/signup-form/signup-form.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, SignupFormComponent],
+  imports: [RouterOutlet, LandingPageComponent, SignupFormComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ title }}!</h1>
     <app-signup-form></app-signup-form>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'SystemWise';
+  title = "SystemWise";
 }
