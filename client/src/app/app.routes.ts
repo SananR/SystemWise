4b3deb58b0abd7ce7472f146b6398c0007a03108
Sanan Rao <raosanan@gmail.com>
2024-07-03 16:40:52 -0400

    Fix frontend error on missing route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { LandingPageComponent } from "./pages/landing-page/landing-page.componen
 import { SignupPageComponent } from "./pages/signup-page/signup-page.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 
-
 export const routes: Routes = [
   { path: "", component: LandingPageComponent },
   { path: "signup", component: SignupPageComponent },
   { path: "login", component: LoginPageComponent },
+  //TODO 404 Not found page
+  { path: "**", component: LandingPageComponent },
 ];
